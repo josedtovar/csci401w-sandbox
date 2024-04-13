c0ed9dc6c7ce53dcb820dd792359e7f0c9ef556b
emilycabral <88002749+emilycabral@users.noreply.github.com>
2024-04-13 05:26:06 -0400

    Basic frontend/backend file structure/design for SmartLearnChat.

---
+++ SmartLearnChat/backend/src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+//connections and listeners
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase().then(() => {
+    app.listen(5000, () => console.log("Server Open & Connected to Database"));
+}).catch(err=>console.log(err));
---
+++ SmartLearnChat/backend/src/routes/user-routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { getAllUsers, userSignup, userLogin } from "../controllers/user-controllers.js";
+import {
+    loginValidator,
+    signupValidator,
+    validate,
+  } from "../utils/validators.js";
+
+
+const userRoutes = Router();
+
+userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
+userRoutes.post("/login", validate(loginValidator), userLogin);
+
+export default userRoutes;
